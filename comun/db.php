d54677ce88f0fa75d2d01c1e398cc066afa059e4
ZoeGeop <zoegeop@gmail.com>
2020-07-12 17:48:05 -0500

    first commit api

--- comun/db.php
+++ comun/db.php
@@ -57,39 +57,14 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 
 
 //Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-$active_group = 'default';
-$query_builder = TRUE;
-
-$enlace['default'] = array(
-    'dsn'    => '',
-    'hostname' => $cleardb_server,
-    'username' => $cleardb_username,
-    'password' => $cleardb_password,
-    'database' => $cleardb_db,
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
-
-$enlace = new mysqli($enlace['default']);
+$enlace = new mysqli($server, $username, $password, $db);
 //echo "Success: A proper connection to MySQL was made! The  database is great." . PHP_EOL;
 //echo "Host information: " . mysqli_get_host_info($enlace) . PHP_EOL;
 
